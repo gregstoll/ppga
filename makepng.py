37acf89630905ac063d06973df47f787d2f9fe5c
Greg Stoll <greg@gregstoll.com>
2007-07-11 04:35:07 +0000

    working on a few, using json modified to take a single quote
    git-svn-id: file:///usr/local/svnrepository/ppga@2032 1105561e-45e8-0310-ac4d-b20c3b09defe

--- makepng.py
+++ makepng.py
@@ -2,18 +2,39 @@
 
 import pngcanvas
 import cgi
-import json
+import jsonsinglequote
+import math
 import cgitb; cgitb.enable()
 
 def fn(x, y):
     return [y for i in range(0, 3)]
 
+# Mapping strategies ensure the result is coerced to [-1,1]
+# 'c' means clip to [-1,1]
+# 'w' means wrap to [-1,1]
+def makeMappingStrategy(jsonObj):
+    return {
+            'c': (lambda v: (v > 1 and 1 or (v < -1 and -1 or v))),
+            'w': (lambda v: (v == 1 and 1 or ((v+1)%2.0)-1))
+    }[jsonObj['m']]
 
+def makeMappingStrategyList(jsonObj):
+    return {
+            'c': (lambda l: [(v > 1 and 1 or (v < -1 and -1 or v)) for v in l]),
+            'w': (lambda l: [(v == 1 and 1 or ((v+1)%2.0)-1) for v in l])
+    }[jsonObj['m']]
+
+
+# TODO - atan might be horribly slow?
 def makeEvaluator(jsonObj):
     return {
        'num': (lambda jo: (lambda x, y: [jo['val'] for i in range(0,3)])),
        'x': (lambda jo: (lambda x, y: [x for i in range(0,3)])),
        'y': (lambda jo: (lambda x, y: [y for i in range(0,3)])),
+       #'atan': (lambda jo: (lambda x, y: [makeMappingStrategy(jo)(math.atan(makeEvaluator(jsonObj['ch'][0])(x,y))) for i in range(0,3)]))
+       'atan': (lambda jo: (lambda x, y: [makeMappingStrategy(jo)(math.atan(v)) for v in makeEvaluator(jsonObj['ch'][0])(x,y)])),
+       'add': (lambda jo: (lambda x, y: [makeMappingStrategy(jo)(v1 + v2) for (v1,v2) in zip(makeEvaluator(jsonObj['ch'][0])(x,y), makeEvaluator(jsonObj['ch'][1])(x,y))])),
+       #'add': (lambda jo: (lambda x, y: [makeMappingStrategy(jo)(v1+v2) for (v1,v2) in makeEvaluator(jsonObj['ch'][0])(x,y)]))
     }[jsonObj['t']](jsonObj)
             
 #if (jsonObj.type == 'number'):
@@ -24,7 +45,7 @@ def makeEvaluator(jsonObj):
 form = cgi.FieldStorage()
 testJson = {'t':'num', 'val':.8}
 #print json.write(testJson)
-func = json.read(form.getfirst('func'))
+func = jsonsinglequote.read(form.getfirst('func'))
 #fn = makeEvaluator(func)
 #print func
 width = int(form.getfirst('width'))
